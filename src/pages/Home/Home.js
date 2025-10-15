b3bc6cd32bb1c34f94dac17236027e9685068f90
TomasEzequielMoura <tomasemoura@gmail.com>
2025-10-14 21:40:03 -0300

    Cambio en el link de redireccion en el boton de home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
       <div className="bg">
-        <div className='container-simil-bootstrap full-size'>
-          <div className='center  flex-dir-col padding-sides full-size' style={{ paddingBottom: '6rem' }}>
+        <div className="container-simil-bootstrap full-size">
+          <div
+            className="center  flex-dir-col padding-sides full-size"
+            style={{ paddingBottom: "6rem" }}
+          >
             <h1 className="title">¡Bienvenido!</h1>
-            <h2 className="subtitle">¡Sonreí con confianza! Programa tus turnos de manera rápida y sencilla.</h2>
-            <a className="btn-epic" href="https://wiri.la/equipos/cardioalem" target="_blank">
+            <h2 className="subtitle">
+              ¡Sonreí con confianza! Programa tus turnos de manera rápida y
+              sencilla.
+            </h2>
+            <a
+              className="btn-epic"
+              href="https://cardioalem.drapp.com.ar"
+              target="_blank"
+            >
               <div>
                 <span>Solicite su turno</span>
                 <span>Solicite su turno</span>
@@ -17,10 +27,9 @@ const Home = () => {
             </a>
           </div>
         </div>
-
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
